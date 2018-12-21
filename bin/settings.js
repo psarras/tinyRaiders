fe3dd1c9ede90153918466e302757bdcb1e5a005
psarras <psarras.st@gmail.com>
2018-12-21 14:01:07 +0000

    Initial Commit

---
+++ bin/settings.js
@@ -0,0 +1,17 @@
+﻿/**
+ * TINY GENERATED CODE, DO NOT EDIT BY HAND
+ * @project GalaxyRaiders
+ */
+
+var Module = {TOTAL_MEMORY: 16777216};
+
+Module.WSServerURL = "ws://10.2.24.91:17702/";
+Module.ProfilerServerURL = "ws://10.2.24.91:54997/";
+
+/*
+ * Workaround for issue UTINY-1091
+ */
+var ut = ut || {}
+ut.Shared = ut.Shared || {}
+ut.HTML = ut.HTML || {}
+ut.Rendering = ut.Rendering || {}
